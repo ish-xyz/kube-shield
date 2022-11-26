7058e36b05cbc28d5bb0dca4bfea009cb3ed34b4
Luigi Zhou <ishamaraia@gmail.com>
2022-11-26 23:08:04 +0000

    boilerplate

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/RedLabsPlatform/kube-shield/pkg/config"
+	"github.com/sirupsen/logrus"
+	admissionv1 "k8s.io/api/admission/v1"
 )
 
 type Server struct {
@@ -37,7 +41,81 @@ func (s *Server) Run() error {
 
 	fmt.Printf("+%v", s)
 
+	http.HandleFunc("/validate-pods", ServeValidatePods)
+
 	// run tls server
 	err := s.Http.ListenAndServeTLS("", "")
 	return err
+}
+
+// ServeValidatePods validates an admission request and then writes an admission
+// review to `w`
+func ServeValidatePods(w http.ResponseWriter, r *http.Request) {
+	logger := logrus.WithField("uri", r.RequestURI)
+	logger.Debug("received validation request")
+
+	in, err := parseRequest(*r)
+	if err != nil {
+		logger.Error(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	logger.Debug(in)
+
+	// write validation logic
+
+	// adm := admission.Admitter{
+	// 	Logger:  logger,
+	// 	Request: in.Request,
+	// }
+
+	// out, err := adm.ValidatePodReview()
+	// if err != nil {
+	// 	e := fmt.Sprintf("could not generate admission response: %v", err)
+	// 	logger.Error(e)
+	// 	http.Error(w, e, http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// w.Header().Set("Content-Type", "application/json")
+	// jout, err := json.Marshal(out)
+	// if err != nil {
+	// 	e := fmt.Sprintf("could not parse admission response: %v", err)
+	// 	logger.Error(e)
+	// 	http.Error(w, e, http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// logger.Debug("sending response")
+	// logger.Debugf("%s", jout)
+	// fmt.Fprintf(w, "%s", jout)
+}
+
+// parseRequest extracts an AdmissionReview from an http.Request if possible
+func parseRequest(r http.Request) (*admissionv1.AdmissionReview, error) {
+	if r.Header.Get("Content-Type") != "application/json" {
+		return nil, fmt.Errorf("Content-Type: %q should be %q",
+			r.Header.Get("Content-Type"), "application/json")
+	}
+
+	bodybuf := new(bytes.Buffer)
+	bodybuf.ReadFrom(r.Body)
+	body := bodybuf.Bytes()
+
+	if len(body) == 0 {
+		return nil, fmt.Errorf("admission request body is empty")
+	}
+
+	var a admissionv1.AdmissionReview
+
+	if err := json.Unmarshal(body, &a); err != nil {
+		return nil, fmt.Errorf("could not parse admission review request: %v", err)
+	}
+
+	if a.Request == nil {
+		return nil, fmt.Errorf("admission review can't be used: Request field is nil")
+	}
+
+	return &a, nil
 }
