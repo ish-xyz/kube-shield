7bd66283a74fe43e14a15c2a861eb980b60c1e67
ish-xyz <ishamaraia@gmail.com>
2022-11-28 19:04:14 +0000

    Removed support for kubebuilder

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # kube-shield
 A dynamic multi purpose admission webhook 
-
-
-# To render the CRDs run:
-
-`go generate ./...` in this directory.
--- pkg/loader/types.go
+++ pkg/loader/types.go
@@ -9,36 +9,27 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// +resource:path=policy
-// +kubebuilder:object:root=true
-
+// Policy resource definition
 type Policy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// +required
-	Spec PolicySpec `json:"spec,omitempty"`
+	Spec              PolicySpec `json:"spec,omitempty"`
 }
 
 type PolicySpec struct {
-	// +kubebuilder:validation:Enum=AllowAll;DenyAll
 	DefaultBehaviour string `json:"defaultBehaviour"`
 	ApplyOn          struct {
-		// +kubebuilder:validation:UniqueItems=true
 		ApiVersions []string `json:"apiVersions"`
-		// +kubebuilder:validation:UniqueItems=true
-		Kinds []string `json:"kinds"`
+		Kinds       []string `json:"kinds"`
 	} `json:"applyOn"`
 	Policies []struct {
-		Name string `json:"name"`
-		// +kubebuilder:validation:MinItems=1
-		// +kubebuilder:validation:MaxItems=500
+		Name  string  `json:"name"`
 		Rules []*Rule `json:"rules"`
 	} `json:"policies"`
 }
 
 type Rule struct {
-	Field string `json:"field"`
-	// +kubebuilder:validation:Enum=GreaterThan;LowerThan;Equal;NotEqual;Regex
+	Field    string `json:"field"`
 	Operator string `json:"operator"`
 	Value    string `json:"value"`
 }
