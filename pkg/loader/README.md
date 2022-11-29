c90e9ea57114a49527c17f2e65448075c9d6fc23
ish-xyz <ishamaraia@gmail.com>
2022-11-29 23:09:04 +0000

    Fix

--- pkg/loader/README.md
+++
@@ -1,4 +0,0 @@
-TODO:
-- implement informer to watch the resources defined in ${repo}/crds.
-- load the resources in memory to make them available to the webhook.
-- find the better data structure to have time constant access to the policies.
--- pkg/webhook/engine/types.go
+++ pkg/webhook/engine/types.go
@@ -1,1 +1,30 @@
 package engine
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Policy resource definition
+type Policy struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              PolicySpec `json:"spec,omitempty"`
+}
+
+type PolicySpec struct {
+	DefaultBehaviour string `json:"defaultBehaviour"`
+	ApplyOn          struct {
+		ApiVersions []string `json:"apiVersions"`
+		Kinds       []string `json:"kinds"`
+	} `json:"applyOn"`
+	Policies []struct {
+		Name  string  `json:"name"`
+		Rules []*Rule `json:"rules"`
+	} `json:"policies"`
+}
+
+type Rule struct {
+	Field    string `json:"field"`
+	Operator string `json:"operator"`
+	Value    string `json:"value"`
+}
