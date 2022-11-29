1f9bb2609cf330f8ce8effc537b877719328e8c4
Luigi Zhou <luigi.zhou@outlook.com>
2022-11-29 23:00:49 +0000

    new dynamic informer

---
+++ pkg/webhook/informer/factory.go
@@ -0,0 +1,17 @@
+package informer
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/tools/cache"
+)
+
+func NewDynamicInformer(clientset dynamic.Interface, gvr schema.GroupVersionResource) *cache.SharedIndexInformer {
+	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(clientset, time.Minute, metav1.NamespaceAll, nil)
+	informer := factory.ForResource(gvr).Informer()
+	return &informer
+}
