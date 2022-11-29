29d3251ea0772c57986e1127ed608f7cae5d9904
Luigi Zhou <luigi.zhou@outlook.com>
2022-11-29 21:49:41 +0000

    remove duplicate

--- pkg/loader/handler.go
+++ pkg/loader/handler.go
@@ -39,16 +39,3 @@ func (p PolicyHandler) OnDelete(obj interface{}) {
 	fmt.Println(policy)
 	// Add On Add Logic here. We could inject it on the constructor function and call it from here
 }
-
-func (l PolicyLoader) ListByNamespace(namespace string) []*unstructured.Unstructured {
-	objList := l.Informer.GetStore().List()
-	cplList := make([]*unstructured.Unstructured, len(objList))
-	for idx, obj := range objList {
-		unstr, exists := obj.(*unstructured.Unstructured)
-		if !exists {
-			logrus.Fatalln("Error converting object to unstructured")
-		}
-		cplList[idx] = unstr
-	}
-	return cplList
-}
