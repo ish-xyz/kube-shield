8d1bf45a7f1d24b7696bc53c0bcf169d5b6635c9
ish-xyz <ishamaraia@gmail.com>
2022-12-05 01:50:42 +0000

    Add generated CRDs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/RedLabsPlatform/kube-shield/cmd"
 
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile="hack/boilerplate.go.txt" paths="./pkg/apis/..."
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./pkg/apis/... crd:crdVersions=v1 output:crd:dir=./crd-gen
 
 func main() {
 	cmd.Execute()
