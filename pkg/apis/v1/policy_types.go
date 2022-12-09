bb7e7852116da8d6a146c48b16b5cccb50014369
ish-xyz <ishamaraia@gmail.com>
2022-12-09 14:54:28 +0000

    Fix

--- pkg/apis/v1/policy_types.go
+++ pkg/apis/v1/policy_types.go
@@ -50,6 +50,29 @@ type PolicyList struct {
 	Items           []Policy `json:"items"`
 }
 
+type ResourceAddress struct {
+	// +kubebuilder:validation:Required
+	APIVersion string `json:"apiVersion"`
+	// +kubebuilder:validation:Required
+	Kind string `json:"kind"`
+}
+
+type Check struct {
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Pattern=^\$*$
+	Field string `json:"field"`
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Enum=GreaterThan;LowerThan;Equal;NotEqual;Regex;Count
+	Operator string `json:"operator"`
+	// +kubebuilder:validation:Required
+	Value interface{} `json:"value"`
+}
+
+type CheckResult struct {
+	Result bool    `json:"result"`
+	Errors []error `json:"errors"`
+}
+
 func init() {
 	SchemeBuilder.Register(&Policy{}, &PolicyList{})
 }
