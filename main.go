c23c862c9dec6d103badac902474766108101aec
ish-xyz <ishamaraia@gmail.com>
2023-02-05 16:10:24 +0100

    new crds

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 //go:generate echo "!!! IMPORTANT have you removed all interfaces from the CRDs structs?"
 //go:generate sleep 3
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile="hack/boilerplate.go.txt" paths="./pkg/apis/v1/..."
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./pkg/apis/v1/... crd:crdVersions=v1 output:crd:dir=./crds
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./pkg/apis/v1/... crd:crdVersions=v1 output:crd:dir=./config
 func main() {
 	cmd.Execute()
 }
